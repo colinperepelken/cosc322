ad956b7257109884284bf5d81bb0969588e26114
jaredhw <jaredhwright@gmail.com>
2017-02-20 12:52:35 -0800

    Added some error checking to the addAmazon and removeAmazon functions

--- src/gameBoard/Board.java
+++ src/gameBoard/Board.java
@@ -4,24 +4,26 @@ public class Board {
 
 	/*
 	 * Attributes 10x10 board = 100 squares. index(0) = (1,10) index(99) =
-	 * (10,1) 
+	 * (10,1)
 	 * 
 	 * The array indexs map to these board positions
-	 * 00|01|02|03|04|05|06|07|08|09| 
-	 * 10|11|12|13|14|15|16|17|18|19|
-	 * 20|21|22|23|24|25|26|27|28|29| 
-	 * 30|31|32|33|34|35|36|37|38|39|
-	 * 40|41|42|43|44|45|46|47|48|49| 
-	 * 50|51|52|53|54|55|56|57|58|59|
-	 * 60|61|62|63|64|65|66|67|68|69| 
-	 * 70|71|72|73|74|75|76|77|78|79|
-	 * 80|81|82|83|84|85|86|87|88|89| 
-	 * 90|91|92|93|94|95|96|97|98|99|
+	 * 00|01|02|03|04|05|06|07|08|09| 10|11|12|13|14|15|16|17|18|19|
+	 * 20|21|22|23|24|25|26|27|28|29| 30|31|32|33|34|35|36|37|38|39|
+	 * 40|41|42|43|44|45|46|47|48|49| 50|51|52|53|54|55|56|57|58|59|
+	 * 60|61|62|63|64|65|66|67|68|69| 70|71|72|73|74|75|76|77|78|79|
+	 * 80|81|82|83|84|85|86|87|88|89| 90|91|92|93|94|95|96|97|98|99|
 	 * 
 	 */
 
 	// W = White Queen, B = Black Queen, + = Arrow, _ = Empty
 	private char[] board = new char[100];
+
+	// Create final references to be used throughout, can easily be changed
+	// here.
+	final private char EMPTY = '_';
+	final private char BLACKQUEEN = 'B';
+	final private char WHITEQUEEN = 'W';
+	final private char ARROW = '+';
 
 	// Methods
 	public char getSquare(int squareIndex) {
@@ -31,43 +33,60 @@ public class Board {
 	}
 
 	public void newGame() {
+		// Fills the entire board with EMPTY
 		for (int i = 0; i < board.length; i++) {
-			// Fills the entire board with "emptys"
-			board[i] = '_';
-
-			// Adds the Black amazons to the game.
-			addAmazon(false, indexify('a', 7));
-			addAmazon(false, indexify('d', 10));
-			addAmazon(false, indexify('g', 10));
-			addAmazon(false, indexify('j', 7));
-			// Adds the White amazons to the game
-			addAmazon(true, indexify('a', 4));
-			addAmazon(true, indexify('d', 1));
-			addAmazon(true, indexify('g', 1));
-			addAmazon(true, indexify('j', 4));
-
+			board[i] = EMPTY;
 		}
+		
+		// Adds the Black amazons to the game.
+		addAmazon(false, indexify('a', 7));
+		addAmazon(false, indexify('d', 10));
+		addAmazon(false, indexify('g', 10));
+		addAmazon(false, indexify('j', 7));
+		// Adds the White amazons to the game
+		addAmazon(true, indexify('a', 4));
+		addAmazon(true, indexify('d', 1));
+		addAmazon(true, indexify('g', 1));
+		addAmazon(true, indexify('j', 4));
 	}
-	//Adds an Amazon to the board at a specific postions, true means the player is white, false = black.
+
+	// Adds an Amazon to the board at a specific postions, true means the player
+	// is white, false = black.
 	private void addAmazon(boolean playerWhite, int squareIndex) {
-		if (playerWhite) {
-			board[squareIndex] = 'W';
+		if (board[squareIndex] == EMPTY) {
+			if (playerWhite) {
+				board[squareIndex] = WHITEQUEEN;
+			} else {
+				board[squareIndex] = BLACKQUEEN;
+			}
 		} else {
-			board[squareIndex] = 'B';
+			System.out.println("This square is not empty, an amazon cannot be placed here");
 		}
 
 	}
 
-	// public void makeMove(boolean player, startSquare, endSquare, arrowSquare)
-	// {
+	private void removeAmazon(int squareIndex) {
+		if (board[squareIndex] == WHITEQUEEN || board[squareIndex] == BLACKQUEEN) {
+			board[squareIndex] = EMPTY;
+		} else {
+			System.out.println("There is not a queen in your ending position");
+		}
 
-	// }
+	}
+
+	public void makeMove(boolean whitePlayer, int startSquare, int endSquare, int arrowSquare) {
+		if (validateMove(whitePlayer, startSquare, endSquare, arrowSquare) == true) {
+			addAmazon(whitePlayer, endSquare);
+
+		}
+
+	}
 
 	// Creates a printable version of the board (10 row paragraph string)
 	public String toString() {
 		StringBuilder boardState = new StringBuilder();
 		for (int i = 1; i <= board.length; i++) {
-			boardState.append(board[i-1] + "|");
+			boardState.append(board[i - 1] + "|");
 			if (i % 10 == 0) {
 				boardState.append("\n");
 			}
